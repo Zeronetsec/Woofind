e2a58ae635c575414b1a21a5406e69c737111e4b
Zeronetsec <senxhunter996@gmail.com>
2026-07-25 00:53:25 +0700

    modified: .install/installer.sh

--- .install/installer.sh
+++ .install/installer.sh
@@ -31,7 +31,7 @@ function install::installer() {
 
         install::getinstall \
             "command go build -v -o woofind" \
-            "Building: ${GG}woofind${N}"
+            "Compiling: ${GG}woofind${N}"
         cd
     )
 
