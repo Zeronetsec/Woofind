72378a3c5cb2fd27e3a2b326cb4483e9258be14b
Zeronetsec <senxhunter996@gmail.com>
2026-08-30 20:10:00 +0700

    modified: .install/backup.sh
    modified: .install/checker.sh
    
    modified: .install/chmod.sh
    
    modified: .install/color.sh
    
    modified: .install/error.sh
    
    modified: .install/getinstall.sh
    
    modified: .install/inpackages.sh
    
    modified: .install/installer.sh
    
    modified: .install/postins.sh
    
    modified: .install/symlink.sh
    
    modified: .install/zinstall.sh
    
    modified: uninstall.sh

--- .install/installer.sh
+++ .install/installer.sh
@@ -2,18 +2,16 @@ function install::installer() {
     if [[ ! -d "${HOME}/.${targetins}_log" ]]; then
         install::getinstall \
             "command mkdir -p ${HOME}/.${targetins}_log" \
-            "Create directory: ${GG}${HOME}/.${targetins}_log${N}"
+            "Create directory: ${color_GG}${HOME}/.${targetins}_log${color_N}"
     fi
 
     (
         cd "${opt}/${targetins}"
         install::getinstall \
-            "command go mod tidy" \
-            "Retidy: ${GG}${targetins}${N}"
-
-        install::getinstall \
-            "command go build -v -o ${targetins}" \
-            "Compiling: ${GG}${targetins}${N}"
-        cd
+            "
+                command go mod tidy
+                command go build -v -o ${targetins}
+            " \
+            "Compiling: ${color_GG}${targetins}${color_N}"
     )
 }; readonly -f install::installer
--- uninstall.sh
+++ uninstall.sh
@@ -48,19 +48,19 @@ fi
 
 install::getinstall \
     "command rm -rf ${opt}/${targetins}" \
-    "Removing: ${GG}${opt}/${targetins}${N}"
+    "Removing: ${color_GG}${opt}/${targetins}${color_N}"
 
 install::getinstall \
     "command rm -f ${bin}/${targetins}" \
-    "Removing: ${GG}${bin}/${targetins}${N}"
+    "Removing: ${color_GG}${bin}/${targetins}${color_N}"
 
 if [[ -d "${HOME}/.${targetins}_log" ]]; then
     install::getinstall \
         "command rm -rf ${HOME}/.${targetins}_log" \
-        "Removing: ${GG}${HOME}/.${targetins}_log${N}"
+        "Removing: ${color_GG}${HOME}/.${targetins}_log${color_N}"
 fi
 
-echo -e "${GG}[+] ${N}${targetins^} removed!"
+echo -e "${color_GG}[+] ${color_N}${targetins^} removed!"
 
-trap - EXIT
+trap - ERR EXIT
 exit ${?}
